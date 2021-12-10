432aece45ea955c9cbcd26401d3c5fbdd74c8610
EvgenKor <evgen@Mac-mini-Evgen.local>
2021-12-10 17:43:31 +0300

    Semaphore operator Manager

--- contracts/consensus/MutableOperatorManager.sol
+++ contracts/consensus/MutableOperatorManager.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./IOperatorManagerRR.sol";
+import "./IOperatorManager.sol";
 
 import "hardhat/console.sol";
 
